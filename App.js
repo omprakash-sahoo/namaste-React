1ea62a954ed85a04812ab8c95da6e82223bc002c
Om prakash <omprakashsahoo.bdk@gmail.com>
2025-04-30 15:41:20 +0530

    App.js code

---
+++ App.js
@@ -0,0 +1,33 @@
+/*
+*<div id= 'parent'>
+    <div d='child'> 
+        h1
+        h2
+    </div>
+    <div d='child2'> 
+        h1
+        h2
+    </div>
+</div>
+
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am a React Element and a js Object"),
+    React.createElement("h2", {}, "I am a React Element and a js Object"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a React Element and a js Object"),
+    React.createElement("h2", {}, "I am a React Element and a js Object"),
+  ]),
+]);
+
+// let headings = React.createElement(
+//   "h1",
+//   { className: "headings" },
+//   "Hello Omm"
+// );
+console.log(parent);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
